f01421710bd3833a867cb954e7f7020d6eecc64f
zhongqigang <qigangzhong@hotmail.com>
2017-09-15 20:06:48 +0800

    change main file name

--- src/main/java/com/qigang/app/Main.java
+++ src/main/java/com/qigang/app/Main.java
@@ -4,7 +4,7 @@ import com.qigang.model.CustomerLoginLog;
 import com.qigang.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
-public class TestController {
+public class Main {
     public static void main(String[] args) {
         SqlSession session = MyBatisUtil.getSqlSession();
         CustomerLoginLog log = session.selectOne("com.qigang.mapper.loginLogMapper.getLoginLog", 1);
